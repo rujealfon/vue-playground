9abea815f7aa0014944bee8b48f365c635b68699
Ruje Alfon <7766205+rujealfon@users.noreply.github.com>
2025-07-02 23:23:44 +0800

    Enhance Vite configuration by adding 'launchEditor' option to vueDevTools plugin for improved development experience.

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [VueRouter(), vue(), tailwindcss(), vueDevTools()],
+  plugins: [
+    VueRouter(),
+    vue(),
+    tailwindcss(),
+    vueDevTools({
+      launchEditor: 'cursor'
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
