6c2323817c7903ef4f8ae9f27cc20a549812d9c3
Ruje Alfon <7766205+rujealfon@users.noreply.github.com>
2025-07-05 11:20:00 +0800

    chore: update dependencies and devDependencies in package.json
    feat: implement dynamic layout selection in app.vue
    
    refactor: remove HelloWorld component and its test
    
    refactor: remove unused icon components
    
    feat: add default layout component with header and footer
    
    chore: create default header and footer components
    
    refactor: remove the-welcome component and its related items
    
    fix: update index page to display a simple home message

---
+++ src/components/navigation/default-footer.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <footer>
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/">
+            Home
+          </RouterLink>
+          <RouterLink to="/about">
+            About
+          </RouterLink>
+        </nav>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/navigation/default-header.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <header>
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/">
+            Home
+          </RouterLink>
+          <RouterLink to="/about">
+            About
+          </RouterLink>
+        </nav>
+      </div>
+    </header>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
